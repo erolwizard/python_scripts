5528579d4c76aebe1e9ef89e94181ca8e7fcc117
Erol <erolwizard@hotmail.com>
2020-01-18 16:05:47 +0000

    map script instead of iterate through for loop

---
+++ map.py
@@ -0,0 +1,22 @@
+
+'''
+
+Instead of looping we can simply iterate with map
+
+'''
+
+'''
+def times3(numbers):
+    new_array = []
+    for number in numbers:
+        new_array.append(number*3)
+    return (new_array)
+
+print(times3([1,2,3,4,5]))
+'''
+
+
+def times3(numbers):
+    return numbers * 3
+
+print(list(map(times3,[1,2,3,4,5])))
